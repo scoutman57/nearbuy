3d501c88f1f80d703259503094286e85e3e49cc3
bregord <sirbrendang@gmail.com>
2014-11-27 17:31:19 -0500

    An error when moving the circle. Shouldn't be too hard to fix. It's mostly done

--- example.js
+++ example.js
@@ -1,3 +1,15 @@
+
+
+// Use it here to get the value after loading the widget
+// ask the instantiated slider widget to tell you it's current value
+
+var searchAddress ="580 Ash Street Winnipeg MB";
+var searchDistance = 1; 
+
+
+
+
+
 function getCoordinates(address, callback){
 
 	var coordinates;
@@ -20,61 +32,65 @@ function getCoordinates(address, callback){
  	return coordinates;
 }
 
-function printMarker(newmarker){
 
- var marker2 = new google.maps.Marker({ // second marker
-		    position: newmarker,
-		    map: map,
-		    title:"Hello World!"
-		});
-
-}
-
-//given an address, will return the latitude Failed. Doesn't really work
-/*
-function addressToLat(address){ //will do this when people enter their addresses
-
-	//var address = " 580 Ash Street Winnipeg MB";
-	var coordinates = getCoordinates(address, function(coordinates){ //This is the callback function from when we asked for the address
-	var geocodedAddress = new google.maps.LatLng(coordinates[0], coordinates[1]); 
-
-	console.log("The stuff I want: " + geocodedAddress)
-
-	return geocodedAddress
-	});
-	
-
-}
-*/
 //Need a function to convert addresses to geocode 
-function getAddMarkers(){
 
+function getAddMarkers(coordinates){
+
+var contentString = 'I WANT TO BE FORGOTTEN'
 	//syntax is address, coords of address, title of advertisement, 
 var markers = [
-		 ['Add1', new google.maps.LatLng(49,49), ],
-		    ['Add2', new google.maps.LatLng(49,49)],
-		    ['Add3', new google.maps.LatLng(49,49)],
-		    ['Add4', new google.maps.LatLng(49,49)],
-		    ['Add5',new google.maps.LatLng(49,49)]
+		 ['Add1', new google.maps.LatLng(coordinates[0]+0.001, coordinates[1]+0.005), "couch", contentString],
+		    ['Add2', new google.maps.LatLng(coordinates[0]+0.002, coordinates[1]+0.004), "chair",contentString],
+		    ['Add3', new google.maps.LatLng(coordinates[0]+0.003, coordinates[1]+0.003), "tv",contentString],
+		    ['Add4', new google.maps.LatLng(coordinates[0]+0.004, coordinates[1]+0.002), "sink",contentString],
+		    ['Add5',new google.maps.LatLng(coordinates[0]+0.005, coordinates[1]+0.001), "dimensional-rifter",contentString]
 	];
 
 	return markers
 
 }
 
+function getAddMarkersStraight(){
+	var contentString = 'I WANT TO BE FORGOTTEN'
+
+	var markers = [
+['Add1',"-97.17913089999999k, 49.863000299999996", "couch", contentString],
+['Add2',"-97.18013089999999k, 49.863000299999996", "couch", contentString],
+['Add3',"-97.18113089999999k, 49.863000299999996", "couch", contentString],
+['Add4',"-97.18213089999999k, 49.863000299999996", "couch", contentString],
+	];
+
+return markers
+}
+
 
 //should each marker have a small summary, a pic, and a link to an ad
 
 
-function initialize() {
-	var address = "580 Ash Street Winnipeg MB";
+
+//need to pass a Center to it as well.
+function initialize(searchDistance, givenAddress) {
+	//var address = "580 Ash Street Winnipeg MB";
+	
+	//searchAddress = address
+	address = givenAddress
+
 	var coordinates = getCoordinates(address, function(coordinates){ //This is the callback function from when we asked for the address
 	var homeCenter = new google.maps.LatLng(coordinates[0], coordinates[1]); 
-	var searchDistance = 1 //This is where they determine how far they want to look
+	var newSearchDistance = /' searchDistance'/
+	document.getElementById('rangeValue').innerHTML= "<p>" + searchDistance + "</p>"
+	
+	//var searchDistance = 1 //This is where they determine how far they want to look
 	//var center = addressToLat(address)
 	//console.log("The Stuff I have" + addressToLat(address))
 
 //map style
+
+
+ 
+
+
 
 var styles =  [
   {
@@ -91,45 +107,8 @@ var styledMap = new google.maps.StyledMapType(styles,
     {name: "Styled Map"});
 
 
-
-
-
-//Put this into a function
-
- var contentString =  '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-
-
-//var markers = getAddMarkers()
-	var markers = [
-		//syntax is address, coords of address, title of advertisement, 
-		 ['Add1', new google.maps.LatLng(coordinates[0]+0.001, coordinates[1]+0.005), "couch", contentString],
-		    ['Add2', new google.maps.LatLng(coordinates[0]+0.002, coordinates[1]+0.004), "chair",contentString],
-		    ['Add3', new google.maps.LatLng(coordinates[0]+0.003, coordinates[1]+0.003), "tv",contentString],
-		    ['Add4', new google.maps.LatLng(coordinates[0]+0.004, coordinates[1]+0.002), "sink",contentString],
-		    ['Add5',new google.maps.LatLng(coordinates[0]+0.005, coordinates[1]+0.001), "dimensional-rifter",contentString]
-	];
-
-//
+var markers = getAddMarkers(coordinates)
+	console.log(markers)
 
 		var mapOptions = {
          		center: homeCenter,
@@ -149,14 +128,15 @@ var styledMap = new google.maps.StyledMapType(styles,
 //computes the circle 
         	//Defines the circle that shows the distance of the search
 		var areaOfSearch = {
-		      strokeColor: '#FF0000',
+		      strokeColor: '#FF6600',
 		      strokeOpacity: 0.25,
 		      strokeWeight: 2,
-		      fillColor: '#FF0000',
+		      fillColor: '#FF6600',
 		      fillOpacity: 0.15,
 		      map: map,
 		      center: homeCenter,
-		      radius: 100*searchDistance
+		      radius: 1000*searchDistance,
+		      editable: false
 		    };
 		    // Add the circle for this city to the map.
 		    searchCircle = new google.maps.Circle(areaOfSearch);
@@ -172,26 +152,26 @@ var styledMap = new google.maps.StyledMapType(styles,
 //loop to put the markers on the map
             for (i = 0; i < markers.length; i++) {  
         		
-        		console.log(searchCircle)
             	if (searchCircle.getBounds().contains(markers[i][1]) == true) { //If the points are within distance, we add them to the map
-
+console.log(searchCircle + " is printed")
 
         		marker = new google.maps.Marker({
             		position: markers[i][1], //loops through and adds the position of each of these markers
             		map: map,
-            		title: markers[i][2]
+            		title: markers[i][2],
+            		info: markers[i][3]
+
         		});
 
 
         		 var infowindow = new google.maps.InfoWindow({ // creates an infowindow for each marker
-      			content: markers[i][3]
+      			content: markers[i][3]//whatever is in element 3 will be entered 
   			});
 
         			google.maps.event.addListener(marker, 'click', function() {//makes the info window for each marker open
-   			 infowindow.setContent(this.title); //replace this.title with whatever content you want
-   			 infowindow.open(map, this);
+	   			 infowindow.setContent(this.info); //replace this.title with whatever content you want
+	   			 infowindow.open(map, this);
 				});
-        			
         			}
 
         			};
@@ -202,17 +182,46 @@ var styledMap = new google.maps.StyledMapType(styles,
 		    position: homeCenter,
 		    map: map, //can use marker.setMap(map) outside this function too.
 		    title:"Home",
-		    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+		    icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+		    draggable:true,
 		});
 
+        	 google.maps.event.addListener(homeMarker, 'dragend', function() {
+		    
+		    //console.log(homeMarker.getPosition())
+		    
+		    searchCircle.setMap(null)
+		    
+		    areaOfSearch.center = homeMarker.getPosition()
+		    searchCircle = new google.maps.Circle(areaOfSearch);
+
+		    //searchCircle.setCenter(homeMarker.getPosition())
+		    //searchCircle = new google.maps.Circle(areaOfSearch);
+		    initialize(searchDistance, searchCircle.getCenter())
 
 
+
+		  });
 	});    
       }
 //
 
 
-      google.maps.event.addDomListener(window, 'load', initialize); //This is where it's started
+// 0-------------------------------THIS IS WHERE WE DO THE CIRCLE REDRAWING.
+
+
+google.maps.event.addDomListener(window, 'load', initialize(1, searchAddress)); //This is where it's started
+
+
+
+
+
+//google.maps.event.addDomListener(window, 'load', initialize(1, searchAddress)); //This is where it's started
+//need to pass a center as well.
+
+
+
+
 
 
 
