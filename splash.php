e857bf4261a8fdceb03bcc5af1d7e8aea3a8b2e6
Jordan Quan <quan.jordan@gmail.com>
2014-12-01 15:22:37 -0500

    home address added to reg

---
+++ splash.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <title>SPLASH</title>
+ 
+</head>
+
+<body>
+
+	<h1>Logged in successfully</h1>
+	<button>Manage your listings</button>
+	<button>Find out what's nearbuy</button>
+
+</body>
+
+</html>
