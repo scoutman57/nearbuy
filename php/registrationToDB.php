f0905c7972a1acd85d4a71fe576b9bf591f44d6b
bregord <sirbrendang@gmail.com>
2014-12-02 03:15:29 -0500

    registration works now?

--- php/registrationToDB.php
+++ php/registrationToDB.php
@@ -14,18 +14,19 @@ if (!$connection) {
    	 die("Connection failed: " . mysqli_connect_error());
 	}
 
-//$username = $_POST['username'];
-//$password = $_POST['password'];
-//$email = $_POST['email'];
-//$profileID = $_POST['profileID'];
-//$address = $_POST['address'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$profileID = $_POST['profileID'];
+$address = $_POST['address'];
 
+/*
 $username = "f";
 $password = "f";
 $email = "f";
 $profileID = '68991';
 $address = "f";
-
+*/
 echo $address
 
 $keyQuery = "SELECT `encryptionKey` FROM `user` WHERE `id`=$profileID";
@@ -36,8 +37,7 @@ $secretKey = $row['encryptionKey'];
 
 //echo $secretKey;
 
-//$query =  "UPDATE `user` SET (`id`='$profileID',`username`='$username',`password`='$password',`email`='$email',`address`='$address',`encryptionKey`='$secretKey')  WHERE `id`='$profileID'";
-$query = "UPDATE `user` SET `id`=$profileID,`username`='$username',`password`='$password',`email`='$email',`address`='$address',`encryptionKey`='$secretKey' WHERE `id`='$profileID'";
+$query =  "UPDATE `user` SET (`id`='$profileID',`username`='$username',`password`='$password',`email`='$email',`address`='$address',`encryptionKey`='$secretKey')  WHERE `id`='$profileID'";
 
 
 //$query = "INSERT INTO `user`(`id`, `username`,`password`,`email`,) VALUES '$memberID' , '$sessionID' " ;
