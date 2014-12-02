38c1f4d70cfc8380d3c2b219d46cf687f12d0f70
bregord <sirbrendang@gmail.com>
2014-12-02 05:23:20 -0500

    lets see if adding a listing works

--- php/enterListing.php
+++ php/enterListing.php
@@ -1,13 +1,11 @@
 <?php
 
-
 $servername = "near-buy.me";
 $dbuser = 'admin';
 $dbpass = 'password';
 $dbname = "nearbuy";
 
-  $connection = mysqli_connect($servername, $dbuser, $dbpass,$dbname);
-
+ $connection = mysqli_connect($servername, $dbuser, $dbpass,$dbname);
 
 if (!$connection) {
    	 die("Connection failed: " . mysqli_connect_error());
@@ -84,8 +82,5 @@ return  $usernameID;
 
 
 //var latlng = document.getElementById("loguser").value;
-
-
-
 
 ?>
