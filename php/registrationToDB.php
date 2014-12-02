174c182b2dc4faf7108329ba07d5ea4e4749957f
bregord <sirbrendang@gmail.com>
2014-12-02 03:38:16 -0500

    registration works now?

--- php/registrationToDB.php
+++ php/registrationToDB.php
@@ -37,9 +37,9 @@ $secretKey = $row['encryptionKey'];
 
 //echo $secretKey;
 
-$query="UPDATE `user` SET `username`='$username',`password`='$password',`email`='$email',`address`='$address'  WHERE `id`=`$profileID`";
+//$query="UPDATE `user` SET `username`='$username',`password`='$password',`email`='$email',`address`='$address'  WHERE `id`=`$profileID`";
 //$query = "UPDATE `user` SET `id`=$profileID,`username`=$username,`password`=$password,`email`=$email,`address`=$address,`encryptionKey`=$keyQuery WHERE `id`=$profileID";
-
+$query = "UPDATE `user` SET `id`=$profileID,`username`='$username',`password`='$password',`email`='$email',`address`='$address',`encryptionKey`='$secretKey' WHERE `id`='$profileID'";
 
 
 //$query = "INSERT INTO `user`(`id`, `username`,`password`,`email`,) VALUES '$memberID' , '$sessionID' " ;
