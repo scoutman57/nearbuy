ec22ba71d3ae27d7c07e1efca0a2ee81af65aeeb
bregord <sirbrendang@gmail.com>
2014-12-02 08:03:11 -0500

    MILESTONE. DB WORKS

--- php/listing.php
+++ php/listing.php
@@ -33,12 +33,46 @@ var_dump($dbuser);
 //var latlng = document.getElementById("loguser").value;
 
 
+$ad ='
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/genlist.js"></script>
+    <title>LISTING</title>
+</head>
+
+<body>
+
+<div class="adpage">
+    <p id="item">Item: '.$name.'</p>
+    <p id="item">Seller: '.$username.'</p>
+    <p id="price">Price: '.$price.'</p>
+    <p id="loc">Location: '.$address.'</p>
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+
+        Image: 
+            <a href='.$image.' target="_blank">
+                <img id="imglink" src="img/link.png" width="50" height="50">
+            </a>
+    </p>
+    <p id="desc">'.$description.'</p>
+</div>
+
+</body>
+
+</html>';
+
 global $connection,$id, $userID, $username, $title, $description, $image, $price, $address, $latlng;
 
 
 //$query = "INSERT INTO `info`(`id`, `latlng`, `name`, `address`, `description_text`, `imagelink`, `userID`, `price`) VALUES ('$id','$latlng','$title','$address','$description','$image','$userID','$price')";
 
-$query = "INSERT INTO `info`(`id`, `latlng`, `name`, `address`, `description_text`, `imagelink`, `viewcount`, `username`, `price`) VALUES ('$id','$latlng','$title','$address','$description','$image','$price','$username','$price')";
+$query = "INSERT INTO `info`(`id`, `latlng`, `name`, `address`, `description_text`, `imagelink`, `viewcount`, `username`, `price`,`ad`) VALUES ('$id','$latlng','$title','$address','$description','$image','$price','$username','$price','$ad')";
 
 
 mysqli_query($connection, $query);
