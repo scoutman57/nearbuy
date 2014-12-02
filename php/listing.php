a8aaae5c6c60baa170997f9f8d454cbdded7e60c
bregord <sirbrendang@gmail.com>
2014-12-02 05:49:07 -0500

    lets see if adding a listing works

--- php/listing.php
+++ php/listing.php
@@ -25,10 +25,38 @@ $address = $_POST['address'];
 $latlng = $_POST['latlng'];
 $upperBound = pow(2, 20);
 $id = rand(1, $upperBound);
-$userID = getUserID();
-$address = getAddress();
+$userID;
+
+getUserID();
 
 var_dump($dbuser);
 //var latlng = document.getElementById("loguser").value;
+
+
+
+function makeListing(){
+global $connection,$id, $userID, $username, $title, $description, $image, $price, $address, $latlng;
+
+$query = "INSERT INTO `info`(`id`, `latlng`, `name`, `address`, `description_text`, `imagelink`, `userID`, `price`) VALUES ('$id','$latlng','$title','$address','$description','$image','$userID','$price')";
+
+mysqli_query($connection, $query);
+
+echo "IT WORKED";
+
+}
+
+
+function getUserID(){
+global $connection, $username;
+
+$usernameQuery = "SELECT `id` FROM `user` WHERE `username`=$username";
+
+$result = mysqli_query($connection, $usernameQuery);
+$row = $result->fetch_assoc();
+$userID = $row['userID'];
+
+
+}
+makeListing();
 
 ?>
