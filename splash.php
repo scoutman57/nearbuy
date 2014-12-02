7e8a60749a6045ede84e786545a59b74d5a416f2
Jordan Quan <quan.jordan@gmail.com>
2014-12-02 08:25:29 -0500

    1
    1

--- splash.php
+++ splash.php
@@ -5,6 +5,8 @@
 
     <link href="css/style.css" rel="stylesheet">
     <script src="js/jquery-2.1.1.min.js"></script>
+    <script src='varAssign.js'></script>
+    <script src='example.js'></script>
     <title>SPLASH</title>
  
 </head>
@@ -13,8 +15,8 @@
 
 <div class="large">
 	<h1>Login success</h1>
-	<button class="lgbutton" onclick="phptest.php"><img src="img/list.png" class="lgicon">Manage your listings</button>
-	<button class="lgbutton" onclick="gotomap.php"><img src="img/map.png" class="lgicon">Find out what's nearbuy</button>
+	<button class="lgbutton" onclick="gotolist.php"><img src="img/list.png" class="lgicon">Manage your listings</button>
+	<button class="lgbutton" onclick='getAddress()'><img src="img/map.png" class="lgicon">Find out what's nearbuy</button>
 </div>
 
 </body>
